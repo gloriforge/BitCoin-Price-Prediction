f3eaa65a4729bd497659fd7cd5a78ac699bbc479
gloriforge <jonasduncan94@outlook.com>
2025-01-22 06:13:22 -0800

    refactor: Add environment value to crypto storing feature

--- data/api_to_db.py
+++ data/api_to_db.py
@@ -1,31 +1,38 @@
+import os
 from pymongo import MongoClient
 from fetch_data import fetch_crypto_data
 import pandas as pd
+from dotenv import load_dotenv
 
-def fetch_data_to_db(mongo_url, api_url, api_key):
-    client = MongoClient(mongo_url)
-    db = client['crypto_data']
-    collection = db['historical_data']
+load_dotenv()
 
-    try:
-        latest_entry = collection.find_one(sort=[("DATE", -1)])
-        if latest_entry:
-            last_date = pd.to_datetime(latest_entry["DATE"]).strftime('%Y-%m-%d')
-        else:
-            last_date = '2011-03-27'
-        
-        print(f"Fetching data starting from {last_date}...")
-        new_data_df = fetch_crypto_data(api_url, api_key)
+mongo_url = os.getenv('MONGO_URL')
+api_url = os.getenv('API_URL')
+api_key = os.getenv('API_KEY')
 
-        if latest_entry:
-            new_data_df = new_data_df[new_data_df['DATE'] > last_date]
-        
-        if not new_data_df.empty:
-            data_to_insert = new_data_df.to_dict(orient='records')
-            result = collection.insert_many(data_to_insert)
-            print(f"Inserted {len(result.inserted_ids)} new records into MongoDB.")
-        else:
-            print("No new data to insert.")
+client = MongoClient(mongo_url)
+db = client['crypto_data']
+collection = db['historical_data']
 
-    except Exception as e:
-        print(f"An error occured: {e}")
+try:
+    latest_entry = collection.find_one(sort=[("DATE", -1)])
+    if latest_entry:
+        last_date = pd.to_datetime(latest_entry["DATE"]).strftime('%Y-%m-%d')
+    else:
+        last_date = '2011-03-27'
+    
+    print(f"Fetching data starting from {last_date}...")
+    new_data_df = fetch_crypto_data(api_url, api_key)
+
+    if latest_entry:
+        new_data_df = new_data_df[new_data_df['DATE'] > last_date]
+    
+    if not new_data_df.empty:
+        data_to_insert = new_data_df.to_dict(orient='records')
+        result = collection.insert_many(data_to_insert)
+        print(f"Inserted {len(result.inserted_ids)} new records into MongoDB.")
+    else:
+        print("No new data to insert.")
+
+except Exception as e:
+    print(f"An error occured: {e}")
